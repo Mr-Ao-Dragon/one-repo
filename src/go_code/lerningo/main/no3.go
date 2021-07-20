e99975a1027bc046f4f5cf06e57e23b76b13bba4
Mr-Ao-Dragon <17798357175@163.com>
2021-07-20 10:56:07 +0800

    耶

--- src/go_code/lerningo/main/no3.go
+++ src/go_code/lerningo/main/no3.go
@@ -1,11 +1,16 @@
 package main
+
+import "fmt"
+
 var (
-		testecho string
-		true bool
-		no int
-	)
+	testecho string
+	true     bool
+	no       int
+)
+
 func main() {
 	testecho = "hello"
 	true = false
 	no = 114514
+	fmt.Println("hey_bro:", testecho)
 }
