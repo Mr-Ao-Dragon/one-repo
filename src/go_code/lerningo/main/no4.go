e447e6072c0cfe41b8286afaa24f0648a7fdebf9
Mr-Ao-Dragon <17798357175@163.com>
2021-07-24 08:29:39 +0800

    我不想写（

--- src/go_code/lerningo/main/no4.go
+++ src/go_code/lerningo/main/no4.go
@@ -1,13 +1,14 @@
 package main
 
 import "fmt"
+//import "os"
 import "os/exec"
-
-/*func init() {
-	fmt.Println("helllo, there!")
-}*/
+func init() {
+	//os.Setenv("TEST", "test")
+	}
 func main() {
-	cmd := exec.Command("date")
+	//cmd := exec.Command("echo", os.ExpandEnv("$PS1"))
+	cmd := exec.Command("apt","moo")
 	out,_ := cmd.CombinedOutput()
 	fmt.Printf("%s",out)
 }
