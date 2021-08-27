01caf4ccbbe967ba6bb3af6b542bf65c79fc612b
Mr-Ao-Dragon <17798357175@163.com>
2021-08-27 11:20:29 +0800

    这个误差有点神奇

--- src/go_code/lerningo/main/no3.go
+++ src/go_code/lerningo/main/no3.go
@@ -4,13 +4,15 @@ import "fmt"
 
 var (
 	testecho string
-	true     bool
-	no       int
+	true      bool
+	no        int
 )
 
 func main() {
 	testecho = "hello"
 	true = false
 	no = 114514
+	no++
 	fmt.Println("hey_bro:", testecho)
+	fmt.Println("hey_bro:", no)
 }
