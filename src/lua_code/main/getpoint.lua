9ced385ed2b3b63ad024ab1b9b2abcfc48578932
Kilohoflippeed <a1357425655@163.com>
2021-10-01 23:35:01 +0800

    完善了函数

--- src/lua_code/main/getpoint.lua
+++ src/lua_code/main/getpoint.lua
@@ -1,32 +1,44 @@
 local function getPointOuter(xCenter,yCenter,radium,number,starAngle)
-    local flag = number
+    local flag = 1
     local proceAngle = starAngle
     local xlist = {}
     local ylist = {}
     repeat
-        xlist[flag] = xCenter+math.sin(radium*(proceAngle))
-        ylist[flag] = yCenter+math.cos(radium*(proceAngle))
-        proceAngle = proceAngle +2*math.pi/number
-        flag = flag-1
-    until flag == 0
+        xlist[flag] = xCenter+radium*math.cos(proceAngle)
+        ylist[flag] = yCenter+radium*math.sin(proceAngle)
+        proceAngle = proceAngle +(2*math.pi)/number
+        flag = flag + 1
+    until flag > number
     return xlist,ylist
 end
-local function getPointdevide(xCenter,yCenter,radium,number,starAngle,devidenumber)
-    local plist = {}
-    plist['x'],plist['y'] = getPointOuter(xCenter,yCenter,radium,number,starAngle)
-    for key,v in pairs(plist) do
-        local i = 1
-        local listi = 1
-        local pdelist = {}
-        local p1,p2= v[i],v[(i+1)]
+
+local function getthepoint(number,devidenumber,plist)
+    local i = 1
+    local listi = 1
+    local pdelist = {}
+    local p1,p2= plist[i],plist[(i+1)]
+        repeat
+            local kr = 0
             repeat
-                local kr = 1
-                repeat
-                    pdelist[listi] = ((devidenumber-kr)*p1 + kr*p2)/devidenumber
-                    kr = kr + 1
-                until kr == devidenumber
-                i = i+1
-                p1,p2= v[i],v[(i+1)]
-            until i== number
-    end
+                pdelist[listi] = ((devidenumber-kr)*p1 + kr*p2)/devidenumber
+                kr = kr + 1
+                listi = listi + 1
+            until kr == devidenumber
+            i = i+1
+            if i ~= number then
+                p1,p2= plist[i],plist[(i+1)]
+            else
+                p1,p2= plist[i],plist[(1)]
+            end
+        until i > number
+    return pdelist
+end
+
+local function getPointdevide(xCenter,yCenter,radium,number,starAngle,devidenumber)
+    local xlist,ylist = {},{}
+    local xdelist,ydelist = {},{}
+    xlist,ylist = getPointOuter(xCenter,yCenter,radium,number,starAngle)
+    xdelist = getthepoint(number,devidenumber,xlist)
+    ydelist = getthepoint(number,devidenumber,xlist)
+    return xdelist,ydelist
 end
