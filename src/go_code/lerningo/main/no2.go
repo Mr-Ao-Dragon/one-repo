1adea32755117366fa26a724abe1c75cc548722d
MrAoDragon <x123456789fy@outlook.com>
2022-01-14 14:03:25 +0800

    Kill file conflict

--- src/go_code/lerningo/main/no2.go
+++
@@ -1,8 +0,0 @@
-package main
-
-func testPtr(num *int){
-	
-}
-func main() {
-	
-}
--- src/go_code/lerningo/main/no6.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Print(65 * 0.37)
-	fmt.Print("\n")
-	fmt.Print(41 * 365 / 687)
-}
